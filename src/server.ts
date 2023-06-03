88b988eeb397af0d57e149753df796c7eb488a04
Naveen <nikobellic1234560987@gmail.com>
2023-06-03 11:45:28 +0530

    build: integrate cloud functions

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import * as express from 'express';
 import { yoga } from './yoga';
+import * as functions from 'firebase-functions';
+import * as cors from 'cors';
 
 import { PORT } from './utils/constants';
 
 const app = express();
 
+app.use(cors());
 // @ts-expect-error
-app.use(yoga.graphqlEndpoint, yoga);
+app.use('/', yoga);
 
 app.listen(PORT, () => {
-  console.log(`Running a GraphQL server at http://localhost:${PORT}/graphql`);
+  console.log(`Running a GraphQL server at /graphql`);
 });
+
+exports.graphql = functions.https.onRequest(app);
